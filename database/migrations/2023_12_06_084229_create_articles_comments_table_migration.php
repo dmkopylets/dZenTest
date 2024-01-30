d723f0d3c6cf324ceceb514d71ccdceeb25ee418
dmkopylets <dm.kopylets@gmail.com>
2024-01-30 04:22:28 +0200

    fixed migration create table  article_commets

--- database/migrations/2023_12_06_084229_create_articles_comments_table_migration.php
+++ database/migrations/2023_12_06_084229_create_articles_comments_table_migration.php
@@ -10,15 +10,29 @@ class CreateArticlesCommentsTableMigration extends Migration
     {
         Schema::create('articles_comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->foreignId('article_id')->constrained(); // article_id може бути пустим
-            $table->foreignId('user_id')->constrained();
-            $table->integer('parent_id')->nullable();
-            $table->integer('position')->nullable();
+            $table->unsignedBigInteger('article_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->integer('position')->nullable()->default(0);
             $table->text('body');
             $table->softDeletes();
             $table->timestamps();
 
-            $table->foreign('parent_id')
+            $table->index('article_id', 'comment_article_idx');
+            $table->index('article_id', 'comment_user_idx');
+            $table->index('article_id', 'comment_parent_idx');
+
+            $table->foreign('article_id', 'comment_article_fk')
+                ->references('id')
+                ->on('articles')
+                ->onDelete('set null');
+
+            $table->foreign('user_id', 'comment_user_fk')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->foreign('parent_id', 'comment_parent_fk')
                 ->references('id')
                 ->on('articles_comments')
                 ->onDelete('set null');
