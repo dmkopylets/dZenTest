e5a44dd881d85ac23197e3d167564c132d726a2c
dmKopylets <dm.kopylets@gmail.com>
2025-07-28 15:48:49 +0300

    fixing home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,44 @@
 /** @var $posts \Illuminate\Pagination\LengthAwarePaginator */
 ?>
 
-<x-app-layout>
-    <!-- Posts Section -->
-    <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+<x-app-layout meta-title="TheCodeholic Blog"
+              meta-description="Lorem ipsum dolor sit amet, consectetur adipisicing elit">
 
-        @foreach($posts as $post)
-            <x-post-item :post="$post"></x-post-item>
-        @endforeach
+    <div class="container max-w-4xl mx-auto py-6">
 
-        {{$posts->onEachSide(1)->links()}}
 
-    </section>
 
-    <x-sidebar />
+            <!-- Popular 3 post -->
+            <div>
+                <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
+                    Popular Posts
+                </h2>
+                @foreach($posts as $post)
+                <div class="grid grid-cols-4 gap-2 mb-4">
+                    <a href="{{route('view', $post)}}" class="pt-1">
+                        <img src="{{$post->getThumbnail()}}" alt="{{$post->title}}"/>
+                    </a>
+                    <div class="col-span-3">
+                        <a href="{{route('view', $post)}}">
+                            <h3 class="text-sm uppercase whitespace-nowrap truncate">{{$post->title}}</h3>
+                        </a>
+                        <div class="flex gap-4 mb-2">
+                            @foreach($post->categories as $category)
+                            <a href="#" class="bg-blue-500 text-white p-1 rounded text-xs font-bold uppercase">
+                                {{$category->title}}
+                            </a>
+                            @endforeach
+                        </div>
+                        <div class="text-xs">
+                            {{$post->shortBody(10)}}
+                        </div>
+                        <a href="{{route('view', $post)}}" class="text-xs uppercase text-gray-800 hover:text-black">Continue
+                            Reading <i class="fas fa-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+                @endforeach
+            </div>
 
+    </div>
 </x-app-layout>
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,23 @@
+<?php
+/** @var $posts \Illuminate\Pagination\LengthAwarePaginator */
+?>
+
+<x-app-layout :meta-title="'TheUncleDima Blog - Posts by category ' . $category->title"
+              :meta-description="'Posts filtered by category ' . $category->title">
+
+    <div class="container mx-auto flex flex-wrap py-6">
+
+        <!-- Posts Section -->
+        <section class="w-full md:w-2/3  px-3">
+            <div class=" flex flex-col items-center">
+                @foreach($posts as $post)
+                    <x-post-item :post="$post"></x-post-item>
+                @endforeach
+            </div>
+            {{ $posts->links() }}
+        </section>
+
+        <!-- Sidebar Section -->
+        <x-sidebar />
+    </div>
+</x-app-layout>
