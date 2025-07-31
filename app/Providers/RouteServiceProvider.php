8b85d563dc0693f9f5ef57c76abb43925e829b93
dmKopylets <dm.kopylets@gmail.com>
2025-07-31 10:44:48 +0300

    added auth buttons

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public const HOME = '/';
+
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+//        $this->configureRateLimiting();
+
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace('App\\Http\\Controllers')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $metaTitle ?: "TheUncleDima`s Blog" }}</title>
-    <meta name="author" content="Uncle Dima">
-    <meta name="description" content="{{ $metaDescription }}">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{ $metaTitle ?: "TheUncleDima`s Blog" }}</title>
+        <meta name="author" content="Uncle Dima">
+        <meta name="description" content="{{ $metaDescription }}">
 
-    <!-- Tailwind -->
-    <link href="{{ asset('css/tailwind.min.css') }}" rel="stylesheet">
-    <style>
-        @import url('{{ asset('css/karlaFonts.css') }}?family=Karla:400,700&display=swap');
+        <style>
+            @import url('{{ asset('css/karlaFonts.css') }}?family=Karla:400,700&display=swap');
+        </style>
 
-        .font-family-karla {
-            font-family: karla;
-        }
+        <!-- Font Awesome -->
+        <script src="{{ asset('js/all.min.js') }}" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
 
-        pre {
-            padding: 1rem;
-            background-color: #1a202c;
-            color: white;
-            border-radius: 0.5rem;
-            margin-bottom: 1rem;
-        }
-    </style>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <!-- AlpineJS -->
-    <script src="{{ asset('js/alpine.min.js') }}" defer></script>
-    <!-- Font Awesome -->
-    <script src="{{ asset('js/all.min.js') }}" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
-</head>
+    </head>
 <body class="bg-gray-50 font-family-karla">
 
 <!-- Text Header -->
@@ -55,14 +42,61 @@
             Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
         </a>
     </div>
-    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-        <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-            <a href="{{route('home')}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
-            @foreach($categories as $category)
-                <a href="{{route('by-category', $category)}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">{{$category->title}}</a>
-            @endforeach
-            <a href="{{route('about-us')}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About us</a>
-        </div>
+    <div :class="(open ? 'block' : 'hidden') + ' w-full flex-grow sm:flex sm:items-center sm:w-auto'">
+        <div
+{{--            class="w-full sm:max-w-full mx-auto flex flex-row flex-wrap sm:flex-row sm:flex-wrap items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">--}}
+            class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-between text-sm font-bold uppercase mt-0 px-6 py-2">
+            <div>
+                <a href="{{route('home')}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
+                @foreach($categories as $category)
+                    <a href="{{route('by-category', $category)}}"
+                       class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">{{$category->title}}</a>
+                @endforeach
+                <a href="{{route('about-us')}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About us</a>
+            </div>
+            <div>
+                @auth
+                    <div class="flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="hover:bg-blue-600 hover:text-white flex items-center rounded py-2 px-4 mx-2">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                @else
+                    <a href="{{route('login')}}"
+                       class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Login</a>
+                    <a href="{{route('register')}}"
+                       class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Register</a>
+                @endauth
+            </div>
+            </div>
     </div>
 </nav>
 
