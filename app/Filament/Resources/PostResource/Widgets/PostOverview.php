0d11aae6e7dc7dc4467cc5210f1da8016ee80172
dmKopylets <dm.kopylets@gmail.com>
2025-08-06 10:13:02 +0300

    fixed filament widget counters views and votes to post view

--- app/Filament/Resources/PostResource/Widgets/PostOverview.php
+++ app/Filament/Resources/PostResource/Widgets/PostOverview.php
@@ -9,7 +9,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostOverview extends Widget
 {
-    protected int | string | array $columnSpan = 3;
+//    protected int | string | array $columnSpan = 3;
+    protected int | string | array $columnSpan = 'full';
 
     public ?Model $record = null;
 
--- resources/views/filament/widgets/post-overview.blade.php
+++ resources/views/filament/widgets/post-overview.blade.php
@@ -1,14 +1,15 @@
-<x-filament::widget >
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <x-dashboard-section class="basis-0 flex-1">
+<x-filament::widget>
+
+    <div class="flex flex-wrap gap-4">
+
+        <x-dashboard-section class="flex-1 min-w-[280px]">
             <h2 class="text-lg sm:text-xl font-bold tracking-tight">Views</h2>
             <div class="flex gap-2 items-center">
                 @svg('heroicon-o-eye', 'h-6 w-6')
                 {{ $viewCount }}
             </div>
         </x-dashboard-section>
-
-        <x-dashboard-section class="basis-0 flex-1">
+        <x-dashboard-section class="flex-1 min-w-[280px]">
             <h2 class="text-lg sm:text-xl font-bold tracking-tight">Upvotes</h2>
             <div class="flex gap-2 items-center">
                 @svg('heroicon-o-hand-thumb-up', 'h-6 w-6')
@@ -16,12 +17,14 @@
             </div>
         </x-dashboard-section>
 
-        <x-dashboard-section class="basis-0 flex-1">
+        <x-dashboard-section class="flex-1 min-w-[280px]">
             <h2 class="text-lg sm:text-xl font-bold tracking-tight">Downvotes</h2>
             <div class="flex gap-2 items-center">
                 @svg('heroicon-o-hand-thumb-down', 'h-6 w-6')
                 {{ $downvotes }}
             </div>
         </x-dashboard-section>
+
     </div>
+
 </x-filament::widget>
