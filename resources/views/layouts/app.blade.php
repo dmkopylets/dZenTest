859323e2c93d8f830be7a41a4a5d3ef20be3eadf
dmKopylets <dm.kopylets@gmail.com>
2025-10-24 11:24:01 +0300

    small fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,7 +106,7 @@
 
 <footer class="w-full border-t bg-white pb-12">
     <div class="w-full container mx-auto flex flex-col items-center">
-        <div class="uppercase pb-6">&copy; myblog.com</div>
+        <div class="uppercase pb-6">&copy; uncle.dima.blog.com</div>
     </div>
 </footer>
 
